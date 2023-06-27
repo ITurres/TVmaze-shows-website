f6b33bbc6624de2454a830da60606b96c022b2d1
iturres <emanueliturres.co@gmail.com>
2023-06-27 20:47:37 -0300

    Add 'create-show-modal.js' -function- -script-

---
+++ src/js/UX/markup-templates/create-show-modal.js
@@ -0,0 +1,43 @@
+const createShowModal = (show) => `
+  <div id="show-info-modal" class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="show__title">${show.name}</h1>
+        <button
+          type="button"
+          class="close close-modal"
+          data-dismiss="modal"
+          aria-label="Close"
+          data-close-modal
+        >
+          <i class="fa-solid fa-xmark fa-beat"></i>
+        </button>
+      </div>
+      <div class="modal-body d-flex gap-3">
+        <img
+          src="${show.image.medium}"
+          alt="show preview"
+          class="rounded"
+        />
+        <div class="p-2">
+          <ul class="nav flex-column">
+            <li class="show__info pb-2">
+              <strong>&#11088;Premiered:</strong><span> ${show.premiered}</span>
+            </li>
+            <li class="show__info pb-2">
+              <strong>&#127909;Genres:</strong><span> ${show.genres}</span>
+            </li>
+            <li class="show__info pb-2">
+              <strong>&#127887;Language:</strong><span> ${show.language}</span>
+            </li>
+            <li class="show__info">
+              <strong>&#128203;Type:</strong><span> ${show.type}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <p class="show__summary p-3">${show.summary.replace(/<p>/, '').replace(/<\/p>/, '')}</p>
+    </div>
+  </div>`;
+
+export default createShowModal;
