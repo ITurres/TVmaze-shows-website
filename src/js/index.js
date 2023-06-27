d25d0cec84f6375e0ffccdbfa62a1cdb6bfa48c2
iturres <emanueliturres.co@gmail.com>
2023-06-27 20:51:54 -0300

    Import 'user-comment-events.js' -script- into 'index.js' -script-

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../styles/index.css';
 import navbarLogo from '../assets/media/tvm-header-logo-red.png';
+import './user-controller/user-comment-events.js';
 
 document.querySelector('#navbar-logo').src = navbarLogo;
