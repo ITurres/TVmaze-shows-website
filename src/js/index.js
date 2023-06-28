3f783f72ca2bacbdeae7f03c1f878492c7526575
Ruthmy <Ruryemo.exe@gmail.com>
2023-06-28 13:57:09 -0600

    Updated the 'index.js' to add the 'renderShows.js'

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../styles/index.css';
 import navbarLogo from '../assets/media/tvm-header-logo-red.png';
+import './UX/renderShows.js';
 
 document.querySelector('#navbar-logo').src = navbarLogo;
