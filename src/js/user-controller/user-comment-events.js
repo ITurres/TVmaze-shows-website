540a9ae52edd69562d818a10b714253b46f13f9f
iturres <emanueliturres.co@gmail.com>
2023-06-28 14:50:02 -0300

    Imported/Added -functions- to handle 'comments injection' -feature- when 'comment button event'

--- src/js/user-controller/user-comment-events.js
+++ src/js/user-controller/user-comment-events.js
@@ -1,5 +1,7 @@
 import getSingleShowData from '../services/tvmaze-API/get-single-show-data.js';
 import createShowModal from '../UX/markup-templates/create-show-modal.js';
+import getComments from '../services/involvement-API/get-comments.js';
+import createComments from '../UX/markup-templates/create-comments.js';
 import closeModal from '../UX/close-modal.js';
 
 // ? 'comment' button event listener
@@ -10,6 +12,12 @@ document.querySelectorAll('[data-show-modal-btn]').forEach((button) => {
   button.addEventListener('click', async () => {
     const showData = await getSingleShowData(button.id);
     modalHolder.innerHTML = createShowModal(showData);
+
+    const showComments = await getComments(showData.id);
+    const commentsTemplate = createComments(showComments);
+    const commentsContainer = document.getElementById('comments-container');
+    commentsContainer.innerHTML = commentsTemplate;
+
     closeModal(modalHolder);
   });
 });
