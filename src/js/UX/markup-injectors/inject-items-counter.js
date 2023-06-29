977000513f527d4cf7162bb82a1818ee511fd984
Ruthmy <Ruryemo.exe@gmail.com>
2023-06-29 15:42:13 -0600

    Added error handling in the 'amountOfShows' function in the file 'inject-items-counter.js'.

--- src/js/UX/markup-injectors/inject-items-counter.js
+++ src/js/UX/markup-injectors/inject-items-counter.js
@@ -1,5 +1,16 @@
 // Updates the number of shows displayed in the homepage counter.
+
 const amountOfShows = (counter) => {
+  // Check if the counter is a number.
+  if (!Number.isFinite(counter)) {
+    throw new Error('The counter must be a number.');
+  }
+
+  // Check if the counter is negative.
+  if (counter < 0) {
+    throw new Error('The counter cannot be negative.');
+  }
+
   const spanCounterElement = document.querySelector('#showsCounter');
   spanCounterElement.textContent = counter;
 };
