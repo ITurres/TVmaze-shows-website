dc2680569fc81f638a49d249a715e1d427470974
iturres <emanueliturres.co@gmail.com>
2023-06-28 23:26:39 -0300

    Added 'commentEvent' -function- to 'user-comment-events.js' -script-

--- src/js/user-controller/user-comment-events.js
+++ src/js/user-controller/user-comment-events.js
@@ -2,8 +2,34 @@ import getSingleShowData from '../services/tvmaze-API/get-single-show-data.js';
 import createShowModal from '../UX/markup-templates/create-show-modal.js';
 import injectComments from '../UX/markup-injectors/inject-comments.js';
 import closeModal from '../UX/close-modal.js';
+import postComment from '../services/involvement-API/post-comment.js';
 
 // ? 'comment' button event listener
+
+const commentEvent = () => {
+  document
+    .querySelector('[data-comment-btn]')
+    .addEventListener('click', async (button) => {
+      const userName = document.querySelector('[name="userName"]');
+      const userComment = document.querySelector('[name="comment"]');
+      const showId = button.target.id;
+
+      if (!userName.value || !userComment.value) {
+        return;
+      }
+
+      await postComment({
+        item_id: showId,
+        username: userName.value,
+        comment: userComment.value
+      });
+
+      await injectComments(showId);
+
+      userName.value = '';
+      userComment.value = '';
+    });
+};
 
 const modalEvents = () => {
   document.querySelectorAll('[data-show-modal-btn]').forEach((button) => {
@@ -12,7 +38,10 @@ const modalEvents = () => {
       const showData = await getSingleShowData(button.id);
       modalHolder.innerHTML = createShowModal(showData);
 
-      injectComments(showData.id);
+      await injectComments(button.id);
+
+      commentEvent();
+
       closeModal(modalHolder);
     });
   });
