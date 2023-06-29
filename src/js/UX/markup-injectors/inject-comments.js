d424897260251ae4dbe69c244612b98b59bec7a3
iturres <emanueliturres.co@gmail.com>
2023-06-28 23:25:21 -0300

    Modify 'injectComments' -async-function- to handle -errors-

--- src/js/UX/markup-injectors/inject-comments.js
+++ src/js/UX/markup-injectors/inject-comments.js
@@ -2,11 +2,15 @@ import getComments from '../../services/involvement-API/get-comments.js';
 import createComments from '../markup-templates/create-comments.js';
 
 const injectComments = async (showId) => {
-  const showComments = await getComments(showId);
-  const commentsTemplate = createComments(showComments);
-  const commentsContainer = document.getElementById('comments-container');
-  commentsContainer.innerHTML = commentsTemplate;
-  return commentsContainer;
+  try {
+    const showComments = await getComments(showId);
+    const commentsTemplate = createComments(showComments);
+    const commentsContainer = document.getElementById('comments-container');
+    commentsContainer.innerHTML = commentsTemplate;
+    return commentsContainer;
+  } catch (error) {
+    return `Could not get comments ${error}`;
+  }
 };
 
 export default injectComments;
