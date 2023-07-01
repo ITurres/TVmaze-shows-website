f094214b3e514cdc0e0b5c73155ef57528a2cb49
Ruthmy <Ruryemo.exe@gmail.com>
2023-06-30 18:20:52 -0600

    Modified the 'getLikes.js' file to remove unnecessary -eslint-disable- comments.

--- src/js/services/involvement-API/getLikes.js
+++ src/js/services/involvement-API/getLikes.js
@@ -1,7 +1,6 @@
 import involvementAPI from './involvement-API.js';
 
 // Function to get the likes of each Show  -----------------------------------------------------
-// eslint-disable-next-line consistent-return
 const getLikesData = async () => {
   const response = await fetch(`${involvementAPI.baseUrl}${involvementAPI.appId}/likes/`, { method: 'GET' });
   const data = await response.json();
